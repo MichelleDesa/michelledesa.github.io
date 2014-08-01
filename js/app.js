7c067a513957ea0e3e5059e80be983d245878725
Michelle Desa <michdesa.a@gmail.com>
2014-08-01 11:51:20 +0530

    Add backstretch jQuery plugin to resize header image and related styles and JS

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
+// $(document).foundation();
+
+/* Author: Sumit Paul */
+jQuery(document).ready(function(e) {
+    var w = e(window).width(),
+        h = e(window).height(),
+        img = e(".mainImage");
+
+    e(window).resize(function() {
+        w = e(window).width();
+        h = e(window).height();
+        img.css("width", w);
+        img.css("height", h);
+    }).resize();
+
+  $(".mainImage").backstretch("images/me.jpg");
+});
